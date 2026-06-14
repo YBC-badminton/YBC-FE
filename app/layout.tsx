a157e10144113c96b83484eb7edbaef343d740d3
SungJae01 <miamo0426@gmail.com>
2026-06-14 11:36:02 +0900

    홈화면 추가 아이콘 이미지 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <link rel="apple-touch-icon" href="/public/images/mascot.png"></link>
       <body className="antialiased font-sans">
         {/* 여기에 (admin)이나 (main)의 레이아웃이 들어옵니다. */}
         <ToastProvider>
