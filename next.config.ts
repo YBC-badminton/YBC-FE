4f1a54788746364421d1fdc4674b55ece77adddc
SungJae01 <miamo0426@gmail.com>
2026-06-29 20:02:20 +0900

    환경 변수 사용하여 API URL 업데이트

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.API_URL || 'http://localhost:8080'; // 기본값 설정
-
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/proxy/api/:path*",
-        destination: `${API_URL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
       {
         source: "/proxy/:path*",
-        destination: `${API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
