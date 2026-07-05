2773e61b980d57fb6b3be78a294ca0c0ea736b22
SungJae01 <miamo0426@gmail.com>
2026-07-05 20:12:03 +0900

    style: 모바일 전용 푸터 배경색 제거 및 불필요한 너비 조정 삭제

--- app/(main)/reviews/page.tsx
+++ app/(main)/reviews/page.tsx
@@ -230,9 +230,8 @@ export default function ReviewPage() {
                         </div>
                     )}
                 </div>
-                <div className="sm:hidden bg-[#e5e7eb] h-[1px] w-[120%]"></div>
                 {/* 모바일 전용 푸터 섹션 (sm:hidden 속성으로 데스크탑에서는 숨김 처리) */}
-                <div className="sm:hidden p-10 bg-white">
+                <div className="sm:hidden p-10">
                     {/* 로고 이미지 경로는 실제 프로젝트 환경에 맞게 수정해 주세요 */}
                     <img 
                         src="/images/logo.png"
